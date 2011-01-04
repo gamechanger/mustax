76e8ba34b5bd8a426692843970ee4891ee9e8faf
Kiril Savino <aurumaeus@gmail.com>
2011-01-04 09:18:13 -0500

    allow another level of indirection, to more properly mimic the Mustache spec for function properties

---
+++ src/io/gamechanger/mustax/MustacheOperationGenerator.java
@@ -0,0 +1,7 @@
+// MustacheOperationGenerator.java
+
+package io.gamechanger.mustax;
+
+public interface MustacheOperationGenerator {
+    public MustacheOperation generateMustacheOperation( java.util.List context );
+}
